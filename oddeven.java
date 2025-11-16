06d6e0b274f7ba929db40441efb55c539b0db801
pramishbhandari <pramishbhandari100@gmail.com>
2025-11-16 08:23:15 +0545

    hello

---
+++ oddeven.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class oddeven{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+
+        input.close();
+    }
+}
